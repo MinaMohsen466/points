18ca8a1a6ace23e4c478ac3bf3695c830b2f9d11
MinaMohsen466 <minamohsen466@gmail.com>
2024-01-24 10:40:33 -0800

    validation

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ let add_tel = document.getElementById('add_tel')
 let add_point = document.getElementById('add_point')
 let add_Btn = document.getElementById('add_Btn')
 let tbody = document.getElementById('tbody')
+let checkNameData = document.getElementById('checkNameData')
 
 //remove add_data
 function claer_add_data(){
@@ -21,6 +22,7 @@ function claer_add_data(){
     add_name.value = ''
 }
 
+//creat array
 let arr;
 if(localStorage.customer_data != null){
     arr = JSON.parse(localStorage.customer_data)
@@ -32,9 +34,8 @@ let mood = "creat";
 let tem;
 
 //add_customer
-
 add_Btn.onclick = function(){
-    if(add_name.value !="" && add_name.value !="" && add_name.value !=""){
+    if(add_name.value !="" && add_name.value !="" && add_name.value !="" && checkNameData.innerHTML == "valid Name"){
         if(mood == "creat"){
             let new_person = {
                 name:add_name.value.toLowerCase(),
@@ -71,8 +72,8 @@ function showData(){
             <td>${arr[i].name}</td>
             <td>${arr[i].tel}</td>
             <td>${arr[i].points}</td>
-            <td><button id="edit_Data" onclick="update(${i})">Edit</button></td>
-            <td><button id="remove_Data" onclick="delet_item(${i})">Remove</button></td>
+            <td><button id="edit_Data" onclick="update(${i})"><i class="fa-solid fa-pen-to-square"></i></button></td>
+            <td><button id="remove_Data" onclick="delet_item(${i})"><i class="fa-solid fa-trash"></i></button></td>
         </tr>
         `
         tbody.innerHTML += tableData;
@@ -81,12 +82,14 @@ function showData(){
 showData()
 
 
+//delet item
 function delet_item(i){
         arr.splice(i,1);
         localStorage.customer_data = JSON.stringify(arr);
         tbody.innerHTML = "";
         showData();
 }
+
 
 //update_finction
 function update(i){
@@ -100,6 +103,7 @@ function update(i){
     add_customer.style.display = 'flex'
 }
 
+
 //search_function
 function searchData(value){
     tbody.innerHTML = "";
@@ -108,15 +112,35 @@ function searchData(value){
             tableData = 
             `
             <tr>
-            <td>${i}</td>
+            <td>${i+1}</td>
             <td>${arr[i].name}</td>
             <td>${arr[i].tel}</td>
             <td>${arr[i].points}</td>
-            <td><button id="edit_Data" onclick="update(${i})">Edit</button></td>
-            <td><button id="remove_Data" onclick="delet_item(${i})">Remove</button></td>
+            <td><button id="edit_Data" onclick="update(${i})"><i class="fa-solid fa-pen-to-square"></i></button></td>
+            <td><button id="remove_Data" onclick="delet_item(${i})"><i class="fa-solid fa-trash"></i></button></td>
             <tr>
             `
             tbody.innerHTML += tableData;
             }
+    }
+}
+
+
+// nameChecker
+function nameChecker(){
+    checkNameData.style.display ="block";
+    checkNameData.style.color ="#00D290";
+    for(var i=0; i<arr.length;i++){
+        if(add_name.value == ""){
+            checkNameData.innerHTML="empty"
+            checkNameData.style.color ="#8F1919";    
+        }else if(arr[i].name != add_name.value){
+            checkNameData.innerHTML="valid Name"
+            checkNameData.style.color ="#00D290";
+        }else if(arr[i].name == add_name.value){
+            checkNameData.innerHTML="invalid Name"
+            checkNameData.style.color ="#8F1919";
+            break;
+        }
     }
 }
